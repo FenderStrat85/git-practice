a23e3d3812b734c60368d48db5ce8a0f7361e734
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-09-30 11:23:51 +0100

    develop.js changed

--- develop.js
+++ develop.js
@@ -1,10 +1,10 @@
 let p = 1;
 let q = 2;
 
-function multiply(num1, num2) {
-  return num1*num2
+function divide(num1, num2) {
+  return num1/num2
 }
 
-multiply(p,q)
+divide(p,q)
 
 //Tom loves the Toffees - what a wally
