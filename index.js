0b828994c8f8960831dc0634c6ac662fa1ed0c95
Tommacfie <tommacfie@aol.com>
2021-09-30 12:31:51 +0200

    add tom remote
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let a = 1;
 let b = 2;
 
-function minus (num1, num2) {
-  return num1-num2
+function add (num1, num2) {
+  return num1 + num2
 }
 
-minus(a, b)
+add(a, b)
 
 //Jake has crazy outfits
